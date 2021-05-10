d16308a9277f330b2188ae84300badabdb1ab465
louis gonzague adam <lg.adam@mac.com>
2021-05-10 17:10:19 +0200

    starting longest word

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,16 @@
+class GamesController < ApplicationController
+
+   def new
+    @letters = Array.new(10) { ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+end
+
+
